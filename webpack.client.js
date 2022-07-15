2973879004039617517ed591baa0756d002e2cd8
imsenthil-m <41285771+imsenthil-m@users.noreply.github.com>
2022-07-14 21:44:53 -0700

    changes in package.json for heroku deployment

--- webpack.client.js
+++ webpack.client.js
@@ -21,7 +21,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
-    port: 9000,
+    port: 5000,
     open: true,
     hot: true,
     inline: true,
